ca65705c24cd5fdbfe3b1f4e840f3009b72a3745
huangmingchuan <hmcxxdxx@gmail.com>
2018-08-26 15:40:23 +0800

    merge_sort

--- HmcLib/algorithm.h
+++ HmcLib/algorithm.h
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+
+//插入排序 增量法
 template <typename T>
 void insertion_sort(vector<T>& v)
 {
@@ -18,4 +20,42 @@ void insertion_sort(vector<T>& v)
         }
         v[j + 1] = temp;
     }
+}
+
+template <typename T>
+void merge(vector<T>& v, typename vector<T>::iterator left, typename vector<T>::iterator mid, typename vector<T>::iterator right)
+{
+    vector<T> v1(left, mid+1);
+    vector<T> v2(mid+1, right+1);
+    auto beg1 = v1.begin();
+    auto beg2 = v2.begin();
+    for (auto it = left; it <= right; ++it)
+    {
+        if (beg1 == v1.end())
+            *it = *beg2++;
+        else if (beg2 == v2.end())
+            *it = *beg1++;
+        else if (*beg1 <= *beg2)
+            *it = *beg1++;
+        else
+            *it = *beg2++;
+    }
+}
+
+template <typename T>
+void merge_sort__(vector<T>& v, typename vector<T>::iterator left, typename vector<T>::iterator right)
+{
+    if (left >= right)
+        return;
+
+    auto mid = left + (right - left) / 2;
+    merge_sort__(v, left, mid);
+    merge_sort__(v, mid+1, right);
+    merge(v, left, mid, right);
+}
+//归并排序 分治法
+template <typename T>
+void merge_sort(vector<T>& v)
+{
+    merge_sort__(v, v.begin(), v.end()-1);
 }
--- HmcLib/testcase.cpp
+++ HmcLib/testcase.cpp
@@ -15,4 +15,22 @@ TEST_CASE("algorithm")
         REQUIRE(v1 == r1);
         REQUIRE_FALSE(v1 == r11);
     }
+
+    SECTION("merge_sort")
+    {
+        vector<int> v1{ 3,2,1,5,4 };
+        vector<int> r1{ 1,2,3,4,5 };
+
+        vector<int> v2{ 3,2,1,5,4,7,6,9,8 };
+        vector<int> r2{ 1,2,3,4,5,6,7,8,9 };
+
+        vector<int> r11{ 1,2,3,5,4 };
+
+        merge_sort(v1);
+        merge_sort(v2);
+
+        REQUIRE(v1 == r1);
+        REQUIRE(v2 == r2);
+        REQUIRE_FALSE(v1 == r11);
+    }
 }
